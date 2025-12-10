935111d007c5d9f6682288148c67e6f1d5c170fa
ParanC <paranchoudhury05@gmail.com>
2025-12-10 23:50:45 +0530

    Add simple Flask app for Azure

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print("Devops pipeline test successfull")
-print("App is running automatically")
+from flask import Flask
+
+app= Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Piku (Bhau) on Azure!"
+
+if  __name__=='__main__':
+    app.run()
